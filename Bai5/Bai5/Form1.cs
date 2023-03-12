f957380018bcec0b78765c5ac8f92811bf1e9134
ThiNe <2151160532@e.tlu.edu.vn>
2023-03-12 14:32:13 +0700

    Complete

--- Bai5/Bai5/Form1.cs
+++ Bai5/Bai5/Form1.cs
@@ -20,7 +20,10 @@ namespace Bai5
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
             if (radioButton1.Checked)
+            {
                 txtC.Enabled = false;
+                txtC.Clear();
+            }
             else
                 txtC.Enabled = true;
         }
